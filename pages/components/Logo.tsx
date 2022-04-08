9e6c550552c92583f644c5bbf8fe625c3c2bd41b
Jailson Jr <jailsonorr@gmail.com>
2022-04-08 00:34:14 -0300

    Feat: Created Logo component

--- pages/components/Logo.tsx
+++ pages/components/Logo.tsx
@@ -4,7 +4,7 @@ import styles from '../../styles/Home.module.css'
 const Logo = () => {
   return (
     <div className={styles.logo}>
-        <Image src="/barriga.png" alt="Sr Barriga FII Logo" width={42} height={50} className={styles.logoImage}/>
+        <Image src="/barriga.png" alt="Sr Barriga FII Logo" width={42} height={40} className={styles.logoImage}/>
         <div className={styles.texts}>
             <h1 className={styles.appname}>sr. barriga</h1>
             <span className={styles.subtitle}>fundos imobiliarios</span>
