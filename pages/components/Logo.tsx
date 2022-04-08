2af5dc75d1443ea19282683e7933f11af6b30be9
Jailson Jr <jailsonorr@gmail.com>
2022-04-08 00:29:09 -0300

    Feat: Initial structure

---
+++ pages/components/Logo.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const Logo = () => {
+  return (
+    <div className={styles.logo}>
+        <Image src="/barriga.png" alt="Sr Barriga FII Logo" width={42} height={50} className={styles.logoImage}/>
+        <div className={styles.texts}>
+            <h1 className={styles.appname}>sr. barriga</h1>
+            <span className={styles.subtitle}>fundos imobiliarios</span>
+            <span className={styles.phrase}>pague o aluguel!</span>
+        </div>
+    </div>
+  )
+}
+
+export default Logo;
