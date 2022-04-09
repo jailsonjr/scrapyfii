0934766a164786c4eebecb0ca44a2ab953e67c2f
Jailson Jr <jailsonorr@gmail.com>
2022-04-08 21:27:42 -0300

    Feat: Add logo

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import BarrigaLogo from '../images/barriga.png';
+import BarrigaLogoSVG from '../images/barriga.svg';
 
 const Logo = () => {
   return (
     <div className={styles.logo}>
-        <Image src="/barriga.png" alt="Sr Barriga FII Logo" width={42} height={40} className={styles.logoImage}/>
+        <Image src={BarrigaLogoSVG} alt="Sr Barriga FII Logo" width={53} height={75} className={styles.logoImage}/>
         <div className={styles.texts}>
             <h1 className={styles.appname}>sr. barriga</h1>
             <span className={styles.subtitle}>fundos imobiliarios</span>
