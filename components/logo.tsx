b04a6750dff3dd0a665af16c9ae16ae4d373bf59
Jailson Jr <jailsonorr@gmail.com>
2022-04-13 00:10:38 -0300

    Feat: Fixed logo size

--- components/logo.tsx
+++ components/logo.tsx
@@ -5,12 +5,14 @@ import BarrigaLogoSVG from '../images/barriga.svg';
 const Logo = () => {
   return (
     <div className={styles.logo}>
-        <Image src={BarrigaLogoSVG} alt="Sr Barriga FII Logo" width={53} height={75} className={styles.logoImage}/>
-        <div className={styles.texts}>
-            <h1 className={styles.appname}>sr. barriga</h1>
-            <span className={styles.subtitle}>fundos imobiliarios</span>
-            <span className={styles.phrase}>pague o aluguel!</span>
-        </div>
+      <div className={styles.logoImageWrapper}>
+        <Image src={BarrigaLogoSVG} alt="Sr Barriga FII Logo" layout="fixed" objectFit="contain" width={53} height={75} className={styles.logoImage}/>
+      </div>
+      <div className={styles.texts}>
+        <h1 className={styles.appname}>sr. barriga</h1>
+        <span className={styles.subtitle}>fundos imobiliarios</span>
+        <span className={styles.phrase}>pague o aluguel!</span>
+      </div>
     </div>
   )
 }
