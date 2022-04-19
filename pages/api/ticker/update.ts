9e017ba66b803f9cf1d0bc37b715b788d6bd00ef
Jailson Jr <jailsonorr@gmail.com>
2022-04-18 23:43:29 -0300

    Feat: Fix crontab run in github actions

--- pages/api/ticker/update.ts
+++ pages/api/ticker/update.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  name: string
+  name: string,
+  updateAt: Number
 }
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  res.status(200).json({ name: 'barriga fii' })
+  res.status(200).json({ name: 'barriga fii', updateAt: Date.now() })
 }
