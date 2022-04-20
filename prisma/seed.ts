cffe1f296958827d51fa82b007cc5b1dcc3728ad
Jailson Jr <jailsonorr@gmail.com>
2022-04-20 01:49:50 -0300

    Feat: Add seed tickers

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,10 +1,41 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, tickers_info } from '@prisma/client';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+import { parse } from 'csv/sync';
 
 const prisma = new PrismaClient();
+const fileToRead = resolve('prisma/data', 'tickers_list.csv');
 
+type fiisType = {
+    ticker: string,
+    ticker_type: string,
+    ticker_adm: string
+}
 
-async function main() {
-  console.log(`Seeding finished.`);
+const readFile = (): Array<fiisType> => {
+    const readData = readFileSync(fileToRead);
+    const convertDataToObject = parse(readData, {
+        delimiter: ',',
+        encoding: 'utf8',
+        columns: true
+    });
+    return convertDataToObject;
+}
+
+const main = () => {
+  const listFiis: Array<fiisType> = readFile();
+  listFiis.map( async (tickerData: fiisType) => {
+    await prisma.tickers_info.create({
+        data: {
+            ticker: tickerData.ticker,
+            ticker_type: tickerData.ticker_type,
+            ticker_admin: tickerData.ticker_adm
+        }
+    });
+  });
 };
+
+main();
+
 
 
