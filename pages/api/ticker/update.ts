2c338b75463c213c61bf6406f49062d55c3cb80f
Jailson Jr <jailsonorr@gmail.com>
2022-04-26 01:24:35 -0300

    Feat: fixed method that return Queue

--- pages/api/ticker/update.ts
+++ pages/api/ticker/update.ts
@@ -4,5 +4,5 @@ import TickerQueueModel from "../../../models/TickerQueueModel";
 export default async function handler(req: NextApiRequest,res: NextApiResponse<any>) {
   let responseApi = await TickerQueueModel.getAllQueueTickers();
   console.log(responseApi);
-  res.status(200).json({});
+  res.status(200).json(responseApi);
 }
